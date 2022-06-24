a3498ad296218f5a72d31f270854afbf8975c082
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-23 22:58:02 -0700

    created  method to get the exact node, from where the cycle of the  linked list started

--- src/DetectCycleLL.java
+++ src/DetectCycleLL.java
@@ -1,5 +1,32 @@
 public class DetectCycleLL {
-    
+
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node detectCycleLL(Node head){
+
+        Node slow =head, fast= head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(fast== slow){
+                slow = head;
+                while (slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+
+                }return slow;
+
+            }
+        }return null;
+    }
 
     public static void main(String[] args) {
 
